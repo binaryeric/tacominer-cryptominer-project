ccbff0ed992a39875b50e7784cf88c4245486d1b
Jody7 <jody7777PVP@gmail.com>
2016-08-26 23:33:20 -0700

    todo: do getasync service

--- tacoMiner/src/tacoMiner/util/HTTP.java
+++ tacoMiner/src/tacoMiner/util/HTTP.java
@@ -1,4 +1,20 @@
 package tacoMiner.util;
 
+import java.net.URLEncoder;
+
 public class HTTP {
+    //TODO make this do stuff
+    public static String formatURL(String urlToEncode){
+        try {
+            return URLEncoder.encode(urlToEncode, "UTF-8");
+        }catch(Exception E){
+            System.out.println("URL Encoding Exception: ");
+            E.printStackTrace();
+            return null;
+        }
+    }
+    public static String getAsync(){
+
+        return null;
+    }
 }

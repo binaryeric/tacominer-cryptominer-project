3e80ca0fc8f80b1659a6564628494f2a95c1d05f
Eric <ericwej@gmail.com>
2016-08-29 21:40:36 -0400

    merge hashcheck

--- tacoMiner/src/tacoMiner/process/cuda/HashCheck.java
+++ tacoMiner/src/tacoMiner/process/cuda/HashCheck.java
@@ -1,16 +1,38 @@
 package tacoMiner.process.cuda;
 
 import tacoMiner.process.SubSystem;
-import static jcuda.runtime.JCuda.*;
-import static jcuda.runtime.cudaMemcpyKind.*;
-import jcuda.*;
-import jcuda.runtime.*;
+import jcuda.driver.JCudaDriver;
+import jcuda.driver.*;
+import static jcuda.driver.JCudaDriver.*;
 
-public class ProcessHash implements SubSystem {
+//
+
+
+
+public class HashCheck implements SubSystem {
 
 	@Override
 	public void start() {
 		// TODO Auto-generated method stub
-
+	}
+	
+//	public String compareHash(String input) {
+//	      CUdevice device = new CUdevice();
+//
+//	}
+	
+	public String fromTargetHash(String input, String targetSHA256) {
+		CudaContext context = (CudaContext) Cuda.getInstance().getContext();
+		CUdevice gpu = context.getGPU();
+		
+		// todo: create pointers to new hash and create sub-contexts to process in parallels
+		
+		
+		long nonce = 0;
+		
+		
+		
+		
+		return "";
 	}
 }

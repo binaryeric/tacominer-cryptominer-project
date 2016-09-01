15c7a540722b1558ec53b5c7e72ed5082c9735e2
Eric <ericwej@gmail.com>
2016-08-31 23:45:20 -0400

    synchronized

--- tacoMiner/src/tacoMiner/process/cuda/HashCheck.java
+++ tacoMiner/src/tacoMiner/process/cuda/HashCheck.java
@@ -1,12 +1,30 @@
 package tacoMiner.process.cuda;
 
 import tacoMiner.process.SubSystem;
+import jcuda.driver.JCudaDriver;
+import jcuda.driver.*;
+import static jcuda.driver.JCudaDriver.*;
+
 
 public class HashCheck implements SubSystem {
 
     @Override
     public void start() {
         // TODO Auto-generated method stub
-
     }
+	
+	public String fromTargetHash(String input, String targetSHA256) {
+		CudaContext context = (CudaContext) Cuda.getInstance().getContext();
+		CUdevice gpu = context.getGPU();
+		
+		// todo: create pointers to new hash and create sub-contexts to process in parallels
+		
+		
+		long nonce = 0;
+		
+		
+		
+		
+		return "";
+	}
 }
